a96207cc574c22eef0d85b0b84b27ef5e5441157
Lanitta <lanittad@hotmail.com>
2017-12-06 20:47:58 -0700

    assignment5

--- app/scripts/controllers/albumCtrl.js
+++ app/scripts/controllers/albumCtrl.js
@@ -1,9 +1,6 @@
 (function(){
   function albumCtrl(){
-    this.albums = [];
-   for (var i=0; i < 12; i++) {
-       this.albums.push(angular.copy(albumPicasso));
-   }
+    this.albumData = albumPicasso;
   }
 
   angular
