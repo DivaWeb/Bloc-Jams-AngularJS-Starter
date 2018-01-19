be0d5763c1d43feded6ee4582949c36947712fea
Lanitta <lanittad@hotmail.com>
2018-01-18 18:38:59 -0700

    chcekpointdirectives1

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -1,60 +1,61 @@
 (function(){
-  function seekBar($document)
+  function seekBar(){
 
-  var calculatePercent = function(seekBar, event) {
-    var offsetX = event.pageX - seekBar.offset().left;
-    var seekBarWidth = seekBar.width();
-    var offsetXPercent = offsetX / seekBarWidth;
-    offsetXPercent = Math.max(0, offsetXPercent);
-    offsetXPercent = Math.min(1, offsetXPercent);
-    return offsetXPercent;
-};
+    var calculatePercent = function(seekBar, event) {
+      var offsetX = event.pageX - seekBar.offset().left;
+      var seekBarWidth = seekBar.width();
+      var offsetXPercent = offsetX / seekBarWidth;
+      offsetXPercent = Math.max(0, offsetXPercent);
+      offsetXPercent = Math.min(1, offsetXPercent);
+      return offsetXPercent;
+    };
+
     return{
       templateUrl: '/templates/directives/seek_bar.html',
       replace: true,
       restrict: 'E',
       scope: { },
-link: function(scope, element, attributes) {
-    // directive logic to return
-           scope.value = 0;
-           scope.max = 100;
+      link: function(scope, element, attributes){
+        scope.value = 0;
+        scope.value = 100;
 
-           var seekBar = $(element);
+        var seekBar = $(element);
 
-           var percentString = function () {
-               var value = scope.value;
-               var max = scope.max;
-               var percent = value / max * 100;
-               return percent + "%";
-           };
+        var percentString = function () {
+          var value = scope.value;
+          var max = scope.max;
+          var percent = value / max * 100;
+          return percent + "%";
+        };
 
-           scope.fillStyle = function() {
-               return {width: percentString()};
-           };
+        scope.fillStyle = function() {
+          return {width: percentString()};
+        };
 
-           scope.onClickSeekBar = function(event) {
-             var percent = calculatePercent(seekBar, event);
-             scope.value = percent * scope.max;
-         };
+        scope.onClickSeekBar = function(event) {
+          var percent = calculatePercent(seekBar, event);
+          scope.value = percent * scope.max;
+        };
 
-         scope.trackThumb = function() {
-     $document.bind('mousemove.thumb', function(event) {
-         var percent = calculatePercent(seekBar, event);
-         scope.$apply(function() {
-             scope.value = percent * scope.max;
-         });
-     });
+        scope.trackThumb = function() {
+              $document.bind('mousemove.thumb', function(event){
+                var percent = calculatePercent(seekBar, event);
+                scope.$apply (function(){
+                  scope.value = percent * scope.max;
+                });
+        });
 
-     $document.bind('mouseup.thumb', function() {
-         $document.unbind('mousemove.thumb');
-         $document.unbind('mouseup.thumb');
-     });
- };
-}
+        $document.bind('mouseup.thumb', function(){
+          $document.unbind('mousemove.thumb');
+          $document.unbind(mouseup.thumb);
+        });
+      };
+            }
     };
-}
-   angular
-       .module('blocJams')
-       
-       .directive('seekBar', ['$document', seekBar])
+
+  }
+
+  angular
+      .module('blocJams')
+      .directive('seekBar', ['$document', seekBar]);
 })();
