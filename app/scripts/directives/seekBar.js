31391cffd2df0378dc29c56513f2e299d7c300fd
Lanitta <lanittad@hotmail.com>
2018-01-22 11:53:42 -0700

    directivesassignment2

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -60,20 +60,23 @@
                   notifyOnChange(scope.value);
                 });
 
+                  });
+
+
+
+               $document.bind('mouseup.thumb', function(){
+                    $document.unbind('mousemove.thumb');
+                    $document.unbind(mouseup.thumb);
+                    });
+              };
+
                 var notifyOnChange = function(newValue){
                     if(typeof scope.onChange === 'function') {
                       scope.onChange({value: newValue});
                     }
                 };
-            });
-
-        $document.bind('mouseup.thumb', function(){
-          $document.unbind('mousemove.thumb');
-          $document.unbind(mouseup.thumb);
-        });
+           }
       };
-            }
-    };
 
   }
 
