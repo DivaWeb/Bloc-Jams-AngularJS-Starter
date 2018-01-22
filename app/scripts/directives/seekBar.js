a54705b7d60372c97b7cc17424bfa6ed2a483456
Lanitta <lanittad@hotmail.com>
2018-01-22 07:52:26 -0700

    Update seekBar.js

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -59,13 +59,15 @@
                   scope.value = percent * scope.max;
                   notifyOnChange(scope.value);
                 });
+                
+                }); 
 
                 var notifyOnChange = function(newValue){
                     if(typeof scope.onChange === 'function') {
                       scope.onChange({value: newValue});
                     }
                 };
-            });
+           
 
         $document.bind('mouseup.thumb', function(){
           $document.unbind('mousemove.thumb');
