9a50e906ac4b2fc016f8d0265aae05d0297d9759
Lanitta <lanittad@hotmail.com>
2018-01-22 13:57:09 -0700

    Update seekBar.js

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -62,20 +62,18 @@
                 
                 }); 
 
-                var notifyOnChange = function(newValue){
-                    if(typeof scope.onChange === 'function') {
-                      scope.onChange({value: newValue});
-                    }
-                };
-           
-
-        $document.bind('mouseup.thumb', function(){
+          $document.bind('mouseup.thumb', function(){
           $document.unbind('mousemove.thumb');
           $document.unbind(mouseup.thumb);
         });
       };
+         var notifyOnChange = function(newValue){
+                    if(typeof scope.onChange === 'function') {
+                      scope.onChange({value: newValue});
+                    }
+                };
             }
-    };
+       };
 
   }
 
